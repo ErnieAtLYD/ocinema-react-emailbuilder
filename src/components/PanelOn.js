09fd387d3150f5bac1f16673d4d203ab2185df78
Ernie Hsiung <ernie@whereby.us>
2019-11-04 16:00:17 -0500

    fix: change layout input to select dropdown

--- src/components/PanelOn.js
+++ src/components/PanelOn.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import ReactQuill, { Quill } from 'react-quill';
@@ -8,12 +11,28 @@ import 'react-quill/dist/quill.snow.css';
 Quill.register('modules/blotFormatter', BlotFormatter);
 
 const PanelOn = ({ panelItem, editPanelField, editPanelQuill, hidePanel }) => {
+  const inputLabel = useRef(null);
+  const [labelWidth, setLabelWidth] = useState(0);
+  useEffect(() => { setLabelWidth(inputLabel.current.offsetWidth) }, []);
   return (
     <>
-      <TextField
-        label="Layout type"
+      <InputLabel shrink ref={inputLabel} htmlFor="layouttype-label">
+        Layout type
+      </InputLabel>
+      <Select
+        onChange={(event) => editPanelField(event, 'layout')}
         value={panelItem.layout}
-      />
+        labelWidth={labelWidth}
+        inputProps={{
+          id: 'layouttype-label',
+        }}>
+        <MenuItem value="header">Email Header</MenuItem>
+        <MenuItem value="footer">Email Footer</MenuItem>
+        <MenuItem value="membership-drive">Membership Drive</MenuItem>
+        <MenuItem value="full-bleed-wrapper">Section Header</MenuItem>
+        <MenuItem value="full-bleed-wrapper-2">Content: Full width</MenuItem>
+        <MenuItem value="filmlayout">Content: Film Layout #1</MenuItem>
+      </Select>
       <TextField
         label="Content"
         value={panelItem.content}
